42e2bbda423a9228c0363fa62416367926fa2ea3
shibilmohd13 <shibilmohd130@gmail.com>
2024-02-22 14:13:11 +0530

    allow hosts

--- carousal/settings.py
+++ carousal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-a%&j&q@3wyr6mq)#2k@6=@b#4wo2_4ey$-+dupcmpccj33)$h3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
